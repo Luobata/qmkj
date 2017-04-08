d2314fea0c29f1d9e12291254f94e4548d4c9f80
Luobta <602975805@qq.com>
2017-04-09 02:31:49 +0800

    add: 场景切换;
    Signed-off-by: Luobta <602975805@qq.com>

--- js/valentine/canvas.js
+++ js/valentine/canvas.js
@@ -1,32 +1,63 @@
 define(function (require, exports, module) {
     var app;
     var content;
+    var background;
+    var background2;
+    var background3;
     var text = [
-        '我们路过高山'
+        {
+            text: '我们路过高山',
+            color: '#b96c40'
+        },
+        {
+            text: '我们路过沙漠',
+            color: '#b5685d'
+        },
+        {
+            text: '我们路过森林',
+            color: '#ffffff'
+        }
     ];
+    var imageArr = [
+    ];
+    var timer = 0;
 
-    function drawText(text) {
+    function drawText(obj) {
         var style = new PIXI.TextStyle({
             fontFamily: 'microsoft yahei',
             fontSize: 21,
-            fill: '#b5685d'
+            fill: obj.color 
         });
-        var richText = new PIXI.Text(text, style);
+        var richText = new PIXI.Text(obj.text, style);
         richText.x = content.width() / 2 - 60;
         richText.y = 102;
 
         app.stage.addChild(richText);
     }
 
+    function walk() {
+        var flag = -1;
+        imageArr.forEach(function (item, index) {
+            //console.log(timer / 1000);
+            //if ((timer / 100000) > 1) {
+            if ((timer % 20) === 1) {
+                if (item.alpha === 1) {
+                    item.alpha = 0;
+                    flag = index;
+                }
+            }
+        });
+        if (flag !== -1) {
+            imageArr[(flag + 1) % 2].alpha = 1;
+        }
+    }
+
     function drawFirst() {
         drawText(text.shift());
-        var imageArr = [
-        ];
         var images = PIXI.Sprite.fromImage('./images/people2.png');
         var images2 = PIXI.Sprite.fromImage('./images/people.png');
         var ticker = PIXI.ticker.shared;
-        var timer = 0;
-        var change = function (timer) {
+        var change = function () {
             var flag = -1;
             imageArr.forEach(function (item, index) {
                 if ((timer % 20) === 1) {
@@ -35,12 +66,11 @@ define(function (require, exports, module) {
                         flag = index;
                     }
                 }
-                    //changeShow();
                 item.width  = item.width * 1.005;
                 item.height = item.height * 1.005;
                 item.x = (content.width() - item.width) / 2;
             });
-            if (flag != -1) {
+            if (flag !== -1) {
                 imageArr[(flag + 1) % 2].alpha = 1;
             }
         };
@@ -68,9 +98,48 @@ define(function (require, exports, module) {
             }
 
             if (images.width < 47) {
-                change(timer);
+                change();
             } else {
                 ticker.stop();
+                ticker.remove();
+                drawSecond(background, background2, './images/desert.png', function (background2) {
+                    drawSecond(background2, background3, './images/forest.png', function () {
+                    });
+                });
+            }
+        });
+        ticker.start();
+    }
+
+    function drawSecond(background, background2, src, fn) {
+        var ticker = PIXI.ticker.shared;
+        ticker.autoStart = false;
+
+        background2 = PIXI.Sprite.fromImage(src);
+        background2.width = app.renderer.width;
+        background2.height = app.renderer.height;
+        background2.alpha = 0;
+
+        app.stage.addChild(background2);
+        background.alpha = 1;
+        imageArr.forEach(function (item) {
+            app.stage.addChild(item);
+        });
+        drawText(text.shift());
+        ticker.stop();
+        timer = 0;
+        ticker.add(function (time) {
+            timer++;
+            walk();
+            if (background.alpha > 0 && background2.alpha < 1) {
+                background.alpha -= 0.01;
+                background2.alpha += 0.01
+            }
+
+            if (timer === 200) {
+                ticker.stop();
+                ticker.remove();
+                fn && fn(background2);
             }
         });
         ticker.start();
@@ -79,7 +148,8 @@ define(function (require, exports, module) {
     // canvas.init
     var init = function (dom) {
         var html = document.body;
-        var background = PIXI.Sprite.fromImage('./images/mountain.png');
+
+        background = PIXI.Sprite.fromImage('./images/mountain.png');
 
         content = dom;
         app = new PIXI.Application(dom.width(), dom.height());
@@ -89,10 +159,6 @@ define(function (require, exports, module) {
         background.height = app.renderer.height;
 
         app.stage.addChild(background);
-
-        var basicText = new PIXI.Text('Basic text in pixi');
-        basicText.x = 30;
-        basicText.y = 90;
 
         drawFirst();
     };
