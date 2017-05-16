5d93783c219cd1e8476e2e30fff5a4814448e33d
Luobta <602975805@qq.com>
2017-05-16 21:19:56 +0800

    add: px2em;
    Signed-off-by: Luobta <602975805@qq.com>

--- px2em/init.js
+++ px2em/init.js
@@ -3,11 +3,23 @@ var path = require('path');
 var px2em = require('./px2em.js');
 
 var resource = '../resource/';
+var cssList = [
+    '../css/index.css',
+    '../css/dream.css',
+    '../css/show.css',
+    '../css/share.css'
+];
 var css = '../css/index.css';
 
 if (!fs.existsSync(resource)) {
     fs.mkdirSync(resource);
 }
-fs.writeFileSync(resource + 'index.css', fs.readFileSync(css), 'utf-8');
-px2em(resource + 'index.css');
+
+cssList.forEach(function (item) {
+    var name = item.split('/').pop();
+    fs.writeFileSync(resource + name, fs.readFileSync(item), 'utf-8');
+    px2em(resource + name);
+})
+// fs.writeFileSync(resource + 'index.css', fs.readFileSync(css), 'utf-8');
+// px2em(resource + 'index.css');
 
