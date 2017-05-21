2132894867e434a20f18eac164e5d989bbc4b518
Luobta <602975805@qq.com>
2017-05-22 00:17:54 +0800

    add: log;
    Signed-off-by: Luobta <602975805@qq.com>

--- js/dream/share-init.js
+++ js/dream/share-init.js
@@ -1,12 +1,13 @@
 define(function(require, exports, module) {
+    var log = require('../valentine/log.js');
     module.exports = function (type, url) {
         var title = '不忘初心•梦想集结号';
         var desc = '和' + window.userInfo.nickname + '一起记录自己最初的梦想，不忘初心从心出发';
         var imgUrl = 'http://h5.ruyiso.com/qmkj/images/dream/icon.jpg';
         var link = url;
         var success = function () {
-            //log('Share');
             _hmt.push(['_trackEvent', 'share', 'share']);
+            log('ShareDream');
         };
         wx.onMenuShareTimeline({
             title: title,
