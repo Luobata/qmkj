60b35f64b75cd7b97ba7dce061593d57f458ceae
Luobta <602975805@qq.com>
2017-05-21 17:02:51 +0800

    fxi: bug;
    Signed-off-by: Luobta <602975805@qq.com>

--- protected/views/dream/show.php
+++ protected/views/dream/show.php
@@ -141,8 +141,10 @@
 <script type="text/javascript" charset="utf-8" src="./js/lib/sea.js"></script>
 <script type="text/javascript">
     var signStr = '<?php echo($signPackage); ?>';
+    var userInfo = '<?php echo($user); ?>';
     var sign = JSON.parse(signStr);
     var dreamList = '<?php ($dreamList); ?>';
+    window.userInfo = JSON.parse(userInfo);
     window.userId = '<?php echo($userId)?>';
     wx.config({
         debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
